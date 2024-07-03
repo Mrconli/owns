c8a76e231e1eca282ee61e6340a14691093118c2
Mrconli <137979819+Mrconli@users.noreply.github.com>
2024-07-03 22:41:57 +0800

    51代理

--- 51代理.py
+++ 51代理.py
@@ -1,6 +1,10 @@
 import requests
+import time
+import random
+from datetime import datetime, timedelta
 
-url = 'https://m.51daili.com/wap/user/signin.html'
+heartbeat_url = 'https://m.51daili.com/wap/user/index.html'
+signin_url = 'https://m.51daili.com/wap/user/signin.html'
 
 headers = {
     'Host': 'm.51daili.com',
@@ -19,9 +23,43 @@ headers = {
 
 data = {}
 
-response = requests.post(url, json=data, headers=headers)
+def send_heartbeat():
+    try:
+        response = requests.post(heartbeat_url, json=data, headers=headers)
+        print(f"{datetime.now()} - Status Code: {response.status_code}")
+    except requests.RequestException as e:
+        print(f"{datetime.now()} - Error")
 
-if response.status_code == 200:
-    print(response.json())
-else:
-    print(f"Error: {response.status_code}, {response.text}")
+def send_signin():
+    try:
+        response = requests.post(signin_url, json=data, headers=headers)
+        if response.status_code == 200:
+            print(response.json())
+        else:
+            print(f"{datetime.now()} - Error")
+    except requests.RequestException as e:
+        print(f"{datetime.now()} - Error")
+
+def get_random_time():
+    now = datetime.now()
+    random_hour = random.randint(0, 23)
+    random_minute = random.randint(0, 59)
+    random_second = random.randint(0, 59)
+    random_time = now.replace(hour=random_hour, minute=random_minute, second=random_second)
+    if random_time < now:
+        random_time += timedelta(days=1)
+    return random_time
+
+next_random_run_time = get_random_time()
+heartbeat_interval = 25 * 60
+
+while True:
+    now = datetime.now()
+    
+    if now >= next_random_run_time:
+        send_signin()
+        next_random_run_time = get_random_time()
+    
+    send_heartbeat()
+    
+    time.sleep(heartbeat_interval)
