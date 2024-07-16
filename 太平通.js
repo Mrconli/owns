a7700f0276d00b2fcf9d08af3aa37d48310843dd
Mrconli <137979819+Mrconli@users.noreply.github.com>
2024-07-16 19:46:11 +0800

    太平通.js

--- 太平通.js
+++ 太平通.js
@@ -15,7 +15,7 @@ export tptCookie="eyJ0xxxxxxxxxxxxxxxxxx#18f5609fe20-xxxxxxxx-xxxxx-xxxxx"
 
 cron: 14 6,18 * * *
 */
-const $ = new Env("太平通-MK集团频道：https://t.me/Bigyellowtl");
+const $ = new Env("太平通");
 
 const _0x433672 = _0x52b7;
 function _0x5de7() {
